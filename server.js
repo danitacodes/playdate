049d6386ff986258ea3fa5a22d312f8fd6f56024
Danita Codes <danitacodes@gmail.com>
2022-12-12 09:39:52 -0800

    pics is finally working

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 
 //EJS views, body-parser, express-static
 app.set('view engine', 'ejs')
-// app.use(express.static("public"))
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static("public"))
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
