4d4e33fb59af048fbd1377ec3029e04e08e6ba3f
Danita Codes <danitacodes@gmail.com>
2022-12-28 07:37:56 -0800

    trying a YouTube suggestion for playfeed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
+const path = require('path')
 
 //require routes
 const mainRoutes = require('./routes/main')
@@ -28,6 +29,10 @@ require('./config/passport')(passport)
 
 //EJS views, body-parser, express-static
 app.set('view engine', 'ejs')
+
+const viewPath = path.join(__dirname, "views")
+app.set("views", viewPath)
+
 app.use(express.static("public"))
 
 app.use(express.urlencoded({ extended: true }))
